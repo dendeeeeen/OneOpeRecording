4e8b2ef4330b75787044d872616c0c79df8556c4
dendeeen <67177554+yukihiro-tanaka@users.noreply.github.com>
2024-01-12 23:25:56 +0000

    cookieによるログイン機構

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_11_073451) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_12_223821) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
   end
 
 end
